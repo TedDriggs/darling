8501b081b6e9eb59b21887572ce201d7b5200deb
Ted Driggs <ted.driggs@outlook.com>
2017-05-23 09:40:26 -0700

    Add darling::util::Flag

--- core/src/util/mod.rs
+++ core/src/util/mod.rs
@@ -1,4 +1,8 @@
 //! Utility types for attribute parsing.
+use std::ops::Deref;
+
+use syn;
+use {FromMetaItem, Result};
 
 mod ident_list;
 mod ignored;
@@ -7,3 +11,38 @@ mod over_ride;
 pub use self::ident_list::IdentList;
 pub use self::ignored::Ignored;
 pub use self::over_ride::Override;
+
+/// Marker type equivalent to `Option<()>` for use in attribute parsing.
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+pub struct Flag(Option<()>);
+
+impl Flag {
+    pub fn present() -> Self {
+        Flag(Some(()))
+    }
+}
+
+impl Deref for Flag {
+    type Target = Option<()>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl FromMetaItem for Flag {
+    fn from_meta_item(mi: &syn::MetaItem) -> Result<Self> {
+        FromMetaItem::from_meta_item(mi).map(Flag)
+    }
+}
+
+impl PartialEq<Option<()>> for Flag {
+    fn eq(&self, rhs: &Option<()>) -> bool {
+        self.0 == *rhs
+    }
+}
+
+impl PartialEq<Flag> for Option<()> {
+    fn eq(&self, rhs: &Flag) -> bool {
+        *self == rhs.0
+    }
+}
