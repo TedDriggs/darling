423a7ae40f40c3e711f9a3f8ad1922c20ff4ec05
Ted Driggs <ted.driggs@outlook.com>
2018-01-26 13:31:02 -0800

    Rename "magic" fields for struct and variant data
    The names now align once again with syn's names for the same fields.

--- core/src/options/from_derive.rs
+++ core/src/options/from_derive.rs
@@ -14,7 +14,7 @@ pub struct FdiOptions {
     /// The field on the target struct which should receive the type generics, if any.
     pub generics: Option<Ident>,
 
-    pub body: Option<Ident>,
+    pub data: Option<Ident>,
 
     pub supports: Option<Shape>,
 }
@@ -25,7 +25,7 @@ impl FdiOptions {
             base: OuterFrom::start(di),
             vis: Default::default(),
             generics: Default::default(),
-            body: Default::default(),
+            data: Default::default(),
             supports: Default::default(),
         }).parse_attributes(&di.attrs)?.parse_body(&di.data)
     }
@@ -48,7 +48,7 @@ impl ParseData for FdiOptions {
     fn parse_field(&mut self, field: &syn::Field) -> Result<()> {
         match field.ident.as_ref().map(|v| v.as_ref()) {
             Some("vis") => { self.vis = field.ident.clone(); Ok(()) }
-            Some("body") => { self.body = field.ident.clone(); Ok(()) }
+            Some("data") => { self.data = field.ident.clone(); Ok(()) }
             Some("generics") => { self.generics = field.ident.clone(); Ok(()) }
             _ => self.base.parse_field(field)
         }
@@ -63,7 +63,7 @@ impl<'a> From<&'a FdiOptions> for codegen::FromDeriveInputImpl<'a> {
             from_ident: Some(v.base.from_ident),
             ident: v.base.ident.as_ref(),
             vis: v.vis.as_ref(),
-            body: v.body.as_ref(),
+            data: v.data.as_ref(),
             generics: v.generics.as_ref(),
             attrs: v.base.attrs.as_ref(),
             forward_attrs: v.base.forward_attrs.as_ref(),
