d982c12558fa8f212a1d4fc4fa5063bcf3618acf
Ted Driggs <ted.driggs@outlook.com>
2019-01-23 10:42:51 -0800

    Use new error implementation in darling-macro

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -10,11 +10,22 @@ use proc_macro::TokenStream;
 
 use darling_core::{codegen, options};
 
+macro_rules! check_errors {
+    ($e:expr) => {
+        match $e {
+            Ok(val) => val,
+            Err(err) => {
+                return err.write_errors().into();
+            }
+        }
+    };
+}
+
 #[proc_macro_derive(FromMeta, attributes(darling))]
 pub fn derive_from_meta(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as syn::DeriveInput);
 
-    let container = options::FromMetaOptions::new(&ast).unwrap();
+    let container = check_errors!(options::FromMetaOptions::new(&ast));
     let trait_impl = codegen::FromMetaImpl::from(&container);
     let result = quote!(#trait_impl);
 
@@ -26,12 +37,11 @@ pub fn derive_from_meta_item(_input: TokenStream) -> TokenStream {
     panic!("darling::FromMetaItem has been replaced by darling::FromMeta");
 }
 
-
 #[proc_macro_derive(FromDeriveInput, attributes(darling))]
 pub fn derive_from_input(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as syn::DeriveInput);
 
-    let container = options::FdiOptions::new(&ast).unwrap();
+    let container = check_errors!(options::FdiOptions::new(&ast));
     let trait_impl = codegen::FromDeriveInputImpl::from(&container);
     let result = quote!(#trait_impl);
 
@@ -42,7 +52,7 @@ pub fn derive_from_input(input: TokenStream) -> TokenStream {
 pub fn derive_field(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as syn::DeriveInput);
 
-    let fdic = options::FromFieldOptions::new(&ast).unwrap();
+    let fdic = check_errors!(options::FromFieldOptions::new(&ast));
     let trait_impl = codegen::FromFieldImpl::from(&fdic);
     let result = quote!(#trait_impl);
 
@@ -53,7 +63,7 @@ pub fn derive_field(input: TokenStream) -> TokenStream {
 pub fn derive_type_param(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as syn::DeriveInput);
 
-    let fdic = options::FromTypeParamOptions::new(&ast).unwrap();
+    let fdic = check_errors!(options::FromTypeParamOptions::new(&ast));
     let trait_impl = codegen::FromTypeParamImpl::from(&fdic);
     let result = quote!(#trait_impl);
 
@@ -64,7 +74,7 @@ pub fn derive_type_param(input: TokenStream) -> TokenStream {
 pub fn derive_variant(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as syn::DeriveInput);
 
-    let fdic = options::FromVariantOptions::new(&ast).unwrap();
+    let fdic = check_errors!(options::FromVariantOptions::new(&ast));
     let trait_impl = codegen::FromVariantImpl::from(&fdic);
     let result = quote!(#trait_impl);
 
