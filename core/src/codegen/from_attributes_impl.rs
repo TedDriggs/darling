d32836df74a26685d9c8288829d7f3ae561e8b38
Ted Driggs <ted.driggs@outlook.com>
2024-07-09 06:25:02 -0700

    Properly forward attrs in FromAttributes
    Fixes #295

--- core/src/codegen/from_attributes_impl.rs
+++ core/src/codegen/from_attributes_impl.rs
@@ -38,6 +38,7 @@ impl ToTokens for FromAttributesImpl<'_> {
             }
         }
 
+        let passed_attrs = self.forward_attrs.as_initializer();
         let inits = self.base.initializers();
         let default = self.base.fallback_decl();
 
@@ -61,6 +62,7 @@ impl ToTokens for FromAttributesImpl<'_> {
                     #default
 
                     ::darling::export::Ok(#ty_ident {
+                        #passed_attrs
                         #inits
                     }) #post_transform
                 }
