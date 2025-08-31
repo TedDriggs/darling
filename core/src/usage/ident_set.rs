9dfd4e0d2f3857275044a5d1610706dc8a4a199e
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-08-31 18:41:03 +0200

    Remove fnv
    `fnv` for use in `HashSet`s was added in #40, over 7 years ago,
    with no real justification.
    This library is made for proc-macros where runtime performance
    doesn't matter as much, considering it will likely run in debug mode.
    
    Remove `fnv` in favor of just using the standard library `HashSet` to
    remove a dependency.
    This is a very small breaking change.

--- core/src/usage/ident_set.rs
+++ core/src/usage/ident_set.rs
@@ -1,8 +1,9 @@
-use fnv::FnvHashSet;
+use std::collections::HashSet;
+
 use syn::Ident;
 
 /// A set of idents.
-pub type IdentSet = FnvHashSet<Ident>;
+pub type IdentSet = HashSet<Ident>;
 
 /// A set of references to idents.
-pub type IdentRefSet<'a> = FnvHashSet<&'a Ident>;
+pub type IdentRefSet<'a> = HashSet<&'a Ident>;
