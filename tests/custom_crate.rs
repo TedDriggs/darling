eee2793ad49d7a22fd6bd0812b9248466332bb6a
Nik Revenco <pm@nikrev.com>
2025-12-18 16:31:06 +0000

    Test for `derive_syn_parse` too

--- tests/custom_crate.rs
+++ tests/custom_crate.rs
@@ -1,53 +1,45 @@
-#![allow(
-    unused,
-    reason = "the fact that this test compiles is enough for it to pass"
-)]
-
 // Override the default location where we search
 mod darling {}
 
 use ::darling::{self as renamed_darling, FromDeriveInput};
 use quote::quote;
-use syn::DeriveInput;
 
 mod inner {
     pub use ::darling as renamed_darling;
 }
 
-#[derive(::darling::FromDeriveInput)]
-#[darling(crate = renamed_darling)]
-struct First {
-    ident: syn::Ident,
+// Renamed to a single module, also test `derive_syn_parse`
+#[derive(::darling::FromMeta)]
+#[darling(crate = renamed_darling, derive_syn_parse)]
+struct InputAttr {
+    foo: ::darling::util::Flag,
 }
 
+// Renamed to a nested module
 #[derive(::darling::FromDeriveInput)]
-#[darling(crate = inner::renamed_darling)]
-struct Second {
+#[darling(attributes(input), crate = inner::renamed_darling)]
+struct Input {
     ident: syn::Ident,
+    my_meta: InputAttr,
 }
 
 #[test]
 fn renamed_darling() {
     let input = quote! {
+        #[input(my_meta(foo))]
         struct FirstInput {
             field: ()
         }
     };
 
     let input = syn::parse2(input).unwrap();
-    let input = First::from_derive_input(&input).unwrap();
+    let input = Input::from_derive_input(&input).unwrap();
     assert_eq!(input.ident, "FirstInput");
+    assert!(input.my_meta.foo.is_present());
 }
 
 #[test]
-pub fn renamed_darling_in_module() {
-    let input = quote! {
-        struct SecondInput {
-            field: ()
-        }
-    };
-
-    let input = syn::parse2(input).unwrap();
-    let input = Second::from_derive_input(&input).unwrap();
-    assert_eq!(input.ident, "SecondInput");
+fn renamed_darling_syn() {
+    let input = syn::parse2::<InputAttr>(quote! { foo }).unwrap();
+    assert!(input.foo.is_present());
 }
