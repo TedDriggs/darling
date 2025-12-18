95edc7586afb990a7c153ade717ca2e40d5f130a
Nik Revenco <pm@nikrev.com>
2025-12-18 15:39:13 +0000

    Move `wrap_in_const` into `codegen`

--- core/src/util/mod.rs
+++ core/src/util/mod.rs
@@ -13,7 +13,6 @@ mod preserved_str_expr;
 mod shape;
 mod spanned_value;
 mod with_original;
-mod wrap_in_const;
 
 pub use self::callable::Callable;
 pub use self::flag::Flag;
@@ -27,4 +26,3 @@ pub use self::preserved_str_expr::PreservedStrExpr;
 pub use self::shape::{AsShape, Shape, ShapeSet};
 pub use self::spanned_value::SpannedValue;
 pub use self::with_original::WithOriginal;
-pub use self::wrap_in_const::wrap_in_const;
