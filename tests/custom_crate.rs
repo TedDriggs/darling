6226d68ea8552906e4db6d53b7f7fc8086cf124c
Nik Revenco <pm@nikrev.com>
2025-12-18 16:07:27 +0000

    test: Parse something into `First` and `Second`

--- tests/custom_crate.rs
+++ tests/custom_crate.rs
@@ -6,7 +6,9 @@
 // Override the default location where we search
 mod darling {}
 
-use ::darling as renamed_darling;
+use ::darling::{self as renamed_darling, FromDeriveInput};
+use quote::quote;
+use syn::DeriveInput;
 
 mod inner {
     pub use ::darling as renamed_darling;
@@ -24,4 +26,28 @@ struct Second {
     ident: syn::Ident,
 }
 
-fn main() {}
+#[test]
+fn renamed_darling() {
+    let input = quote! {
+        struct FirstInput {
+            field: ()
+        }
+    };
+
+    let input = syn::parse2(input).unwrap();
+    let input = First::from_derive_input(&input).unwrap();
+    assert_eq!(input.ident, "FirstInput");
+}
+
+#[test]
+pub fn renamed_darling_in_module() {
+    let input = quote! {
+        struct SecondInput {
+            field: ()
+        }
+    };
+
+    let input = syn::parse2(input).unwrap();
+    let input = Second::from_derive_input(&input).unwrap();
+    assert_eq!(input.ident, "SecondInput");
+}
